635b698821b41f7dba2a365889b9633865abe46a
ArtemTereshkovich <artyomt7777@gmail.com>
2019-12-15 15:53:42 +0300

    add [Serializable] attribute

--- NetworkSocketServer.Shared/Responses/ErrorResponse.cs
+++ NetworkSocketServer.Shared/Responses/ErrorResponse.cs
@@ -1,5 +1,8 @@
-﻿namespace NetworkSocketServer.DTO.Responses
+﻿using System;
+
+namespace NetworkSocketServer.DTO.Responses
 {
+    [Serializable]
     public class ErrorResponse : Response
     {
         public string ErrorText { get; set; }
